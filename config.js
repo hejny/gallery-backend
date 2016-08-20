947d28b8377ebf99b6665ee9fa5bad7eeaa87b6c
Pavol Hejný <me@pavolhejny.cz>
2016-08-20 18:18:47 +0200

    Spliting backend and frontend

---
+++ config.js
@@ -0,0 +1,13 @@
+
+
+module.exports = {
+
+    mongo: {
+        server: 'ds031835.mlab.com:31835/gallery-phejny',
+        user: 'gallery',
+        pass: '.{#^K7{dnkatf3BY*,BuU)f%@E~M3R&)hCRjpB;>qP/(aK-~zHw)q-Qw3sv]nAz#Yeh,b3Dm&6ypTD9)&RAGSfrH3NxHC,-CDdRA4D7J6pqQ{9\\N*](c+pJ"dp5<\'Y+Z2hpRs2CT8*U$Vv+..\\j%J)U;H].x;T5z4YH6;Eb\'YrJ}^j4D3AxP$n]*7;K"r>;UuKhq$cDDjx_>*H"y_cNH%Ktq9z6]yB@:"f;8Z2.:>peY"UL[~wy=*B/_])9pU@5A'
+    },
+
+    port:38567
+
+};
